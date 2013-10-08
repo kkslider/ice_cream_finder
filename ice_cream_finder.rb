2eb3ff118dec1e4fd088e7f1d88d877e9a35fd6e
Benjamin Lee <benjamsandwich@gmail.com>
2013-10-08 14:43:04 -0700

    finished ice cream locator

--- ice_cream_finder.rb
+++ ice_cream_finder.rb
@@ -3,19 +3,24 @@ require 'nokogiri'
 require 'addressable/uri'
 require 'json'
 
+puts "Enter your current location:"
+address = gets.chomp
+
 location = Addressable::URI.new(
 :scheme => "https",
 :host => "maps.googleapis.com",
 :path => "maps/api/geocode/json",
-:query_values => { :address => "1061 Market St, San Francisco, CA",
-                   :sensor => false }
+:query_values => {
+                   :address => "#{address}",
+                   :sensor => false
+                 }
 ).to_s
 
 response = RestClient.get(location)
 json_location = JSON.parse(response)
 
-coordinates = json_location["results"].first["geometry"]["location"].values.join(',')
-# p coordinates
+my_coordinates = json_location["results"].first["geometry"]["location"].values.join(',')
+
 
 vendors = Addressable::URI.new(
 :scheme => "https",
@@ -23,20 +28,47 @@ vendors = Addressable::URI.new(
 :path => "maps/api/place/nearbysearch/json",
 :query_values => {
                   :key => "AIzaSyBtJztsbe5z93ePYrVxj379h8OTvUf520c",
-                  :location => "#{coordinates}",
-                  :radius => 300,
+                  :location => "#{my_coordinates}",
+                  :radius => 1300,
                   :keyword => "ice cream",
                   :sensor => false,
                   :opennow => true
-                 },
+                 }
 ).to_s
 
-p vendors
-# response = RestClient.get(vendors)
-# json_vendors = JSON.parse(response)
-# p json_vendors
+
+response = RestClient.get(vendors)
+json_vendors = JSON.parse(response)
+
+ic_coordinates = json_vendors["results"].first["geometry"]["location"].values.join(',')
+
+directions = Addressable::URI.new(
+:scheme => "https",
+:host => "maps.googleapis.com",
+:path => "maps/api/directions/json",
+:query_values => {
+                  :origin => "#{my_coordinates}",
+                  :destination => "#{ic_coordinates}",
+                  :sensor => false,
+                  :mode => "walking"
+                 }
+).to_s
+
+response = RestClient.get(directions)
+json_directions = JSON.parse(response)
+
+p directions
+
+json_directions["routes"].first["legs"].first["steps"].each do |direction|
+  parsed_direction = Nokogiri::HTML(direction["html_instructions"])
+
+  if parsed_direction.text.include?("Destination")
+    split_directions = parsed_direction.text.split("Destination")
+    puts split_directions.first
+    puts "Destination" + split_directions.last
+  else
+    puts parsed_direction.text
+  end
+end
 
 
-
-
-# puts RestClient.get()
