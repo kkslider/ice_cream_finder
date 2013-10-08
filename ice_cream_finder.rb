8845a4c88f6cd4a6303c5e406e2b011d2c6a8fd3
Benjamin Lee <benjamsandwich@gmail.com>
2013-10-08 13:47:04 -0700

    Query for ice cream vendors

--- ice_cream_finder.rb
+++ ice_cream_finder.rb
@@ -9,17 +9,34 @@ location = Addressable::URI.new(
 :path => "maps/api/geocode/json",
 :query_values => { :address => "1061 Market St, San Francisco, CA",
                    :sensor => false }
-)
+).to_s
 
-puts location.to_s
+response = RestClient.get(location)
+json_location = JSON.parse(response)
 
+coordinates = json_location["results"].first["geometry"]["location"].values.join(',')
+# p coordinates
 
 vendors = Addressable::URI.new(
 :scheme => "https",
 :host => "maps.googleapis.com",
-:path => "maps/api/place/nearbysearch",
-:output => "json",
-:query_values => {:location => "value" },
-)
+:path => "maps/api/place/nearbysearch/json",
+:query_values => {
+                  :key => "AIzaSyBtJztsbe5z93ePYrVxj379h8OTvUf520c",
+                  :location => "#{coordinates}",
+                  :radius => 300,
+                  :keyword => "ice cream",
+                  :sensor => false,
+                  :opennow => true
+                 },
+).to_s
+
+p vendors
+# response = RestClient.get(vendors)
+# json_vendors = JSON.parse(response)
+# p json_vendors
+
+
+
+
 # puts RestClient.get()
-#
